5ab3e3ae117d54952132179dad7a00b4e2a5112e
IT24102678 <it24102678@my.sliit.lk>
2026-04-27 13:59:19 +0530

    third

--- WMT_LabTest_Project/frontend/src/api/itemApi.js
+++ WMT_LabTest_Project/frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://railway.com/project/6d7f9768-8d3a-4eb2-aede-e286ddd40729/service/35483d9f-685a-40cf-a729-e1b7db918e71/settings/#networking",
 });
 
 export const getItems = () => API.get("/items");
